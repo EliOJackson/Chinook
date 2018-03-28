f4c71d60a23313ee94932ef25bcbffdaba2764dc
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-03-28 15:33:58 -0500

    third query now has full name concatted

--- SQL/chinook-queries.sql
+++ SQL/chinook-queries.sql
@@ -9,7 +9,7 @@ FROM Customer c
 WHERE c.country IS "Brazil"
 
 -- Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
-SELECT c.FirstName, c.LastName, i.BillingCountry, i.InvoiceDate, i.InvoiceId
+SELECT (c.FirstName || ' ' || c.LastName) AS FullName, i.BillingCountry, i.InvoiceDate, i.InvoiceId
 FROM Customer c
 JOIN Invoice i ON i.CustomerId=c.CustomerId 
 WHERE c.country IS "Brazil"
